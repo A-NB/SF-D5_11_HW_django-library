7ec8c6a6da04e62500302e70915c36577a8da4fb
A-NB <bel-an510@yandex.ru>
2022-09-21 16:22:43 +0300

    modify settings.ALLOWED_HOSTS

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('p_library_django_SECRET_KEY') #'django-insecure-))h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True)) #DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com'] #ALLOWED_HOSTS = []
 
 
 # Application definition
