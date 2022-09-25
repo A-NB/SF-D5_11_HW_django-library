79b30c0f7afd3400980cee8a0bc909215d030270
A-NB <bel-an510@yandex.ru>
2022-09-25 18:30:12 +0300

    commentback import django_heroku in settings

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -142,6 +142,6 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-# # Configure Django App for Heroku.
-# import django_heroku
-# django_heroku.settings(locals())
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
